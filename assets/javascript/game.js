37063ad9a74086fa2f8a6b16168ea056ae0d5fdb
Raymond Fwu <rfwu@raymonds-imac.attlocal.net>
2019-01-02 18:36:37 -0600

    added some javascript to test dom manipulation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,26 @@
 // This is a 
+
+// Step 1
+// Make all the characters selectable
+
+$("#btn-kenobi").click(function(){
+    $("#btn-kenobi-back").removeClass("bg-white").addClass("bg-success");
+    alert("You've selected Kenobi.");
+});
+
+$("#btn-skywalker").click(function(){
+    alert("You've selected Skywalker.");
+});
+
+$("#btn-sidious").click(function(){
+    alert("You've selected Sidious.");
+});
+
+$("#btn-maul").click(function(){
+    alert("You've selected Maul.");
+});
+
+/*  
+    Step 2
+    Move the selected character's portrait into the "Your Character" section
+*/
