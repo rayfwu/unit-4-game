fdc42b111d588d4095780c537aa224ef26c36e78
Raymond Fwu <rfwu@raymonds-imac.attlocal.net>
2019-01-10 08:26:56 -0600

    completely redid the javascript to be object oriented

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,114 @@
-// This is a 
-
 // Step 1
 // Make all the characters selectable
 
-$("#btn-kenobi").click(function(){
-    $("#btn-kenobi-back").removeClass("bg-white").addClass("bg-success");
-    alert("You've selected Kenobi.");
-});
+// var student = {                 // object name
+//     firstName:"Jane",           // list of properties and values
+//     lastName:"Doe",
+//     age:18,
+//     height:170,
+//     fullName : function() {     // object function
+//        return this.firstName + " " + this.lastName;
+//     }
+// }; 
 
-$("#btn-skywalker").click(function(){
-    alert("You've selected Skywalker.");
-});
+$(document).ready(function(){
 
-$("#btn-sidious").click(function(){
-    alert("You've selected Sidious.");
-});
+    function Character(pChar, pColor, pHP) {
+        this.pChar = pChar,
+        this.pColor = pColor,
+        this.pHP = pHP,
+        this.pID = pChar.attr('id'),
+        this.enemySelect = function() {
+            pChar.appendTo("#enemy-area");
+        }
+    }
 
-$("#btn-maul").click(function(){
-    alert("You've selected Maul.");
+    var kenobi = new Character($("#btn-kenobi"), $("#btn-kenobi-back"), $("#kenobi-hp"));
+    var skywalker = new Character($("#btn-skywalker"), $("#btn-skywalker-back"), $("#skywalker-hp"));
+    var sidious = new Character($("#btn-sidious"), $("#btn-sidious-back"), $("#sidious-hp"));
+    var maul = new Character($("#btn-maul"), $("#btn-maul-back"), $("#maul-hp"));
+
+    var players = [kenobi, skywalker, sidious, maul];
+
+    for (let i = 0; i < players.length; i++) {
+        console.log(players[i].pID);
+        players[i].pChar.click(function() {
+            for (let j = 0; j < players.length; j++) {
+                if (players[j] != players[i]) {
+                    players[j].enemySelect();
+                }
+            }
+        })
+    }
+
+    /*
+
+    //make an array of objects for the buttons and enemies
+    var players = [$("#btn-kenobi"), $("#btn-skywalker"), $("#btn-sidious"), $("#btn-maul")];
+    var enemies = [$("#enemy-kenobi-back"), $("#enemy-skywalker-back"), $("#enemy-sidious-back"), $("#enemy-maul-back")];
+
+    $("#btn-kenobi").click(function(){
+        for (var i = 0; i < players.length; i++) {
+            if (players[i].attr('id') != "btn-kenobi") {
+                //players[i].hide();
+                players[i].appendTo("#enemy-area")
+            }
+        }
+
+        // for (var i = 0; i < enemies.length; i++) {
+        //     if (enemies[i].attr('id') != "enemy-kenobi-back") {
+        //         enemies[i].show();
+        //     }
+        // }
+    });
+
+    $("#btn-skywalker").click(function(){
+        for (var i = 0; i < players.length; i++) {
+            if (players[i].attr('id') != "btn-skywalker") {
+                //players[i].hide();
+                players[i].appendTo("#enemy-area")
+            }
+        }
+
+        // for (var i = 0; i < enemies.length; i++) {
+        //     if (enemies[i].attr('id') != "enemy-skywalker-back") {
+        //         enemies[i].show();
+        //     }
+        // }
+    });
+
+    $("#btn-sidious").click(function(){
+        for (var i = 0; i < players.length; i++) {
+            if (players[i].attr('id') != "btn-sidious") {
+                //players[i].hide();
+                players[i].appendTo("#enemy-area")
+            }
+        }
+
+        // for (var i = 0; i < enemies.length; i++) {
+        //     if (enemies[i].attr('id') != "enemy-sidious-back") {
+        //         enemies[i].show();
+        //     }
+        // }
+    });
+
+    $("#btn-maul").click(function(){
+        for (var i = 0; i < players.length; i++) {
+            if (players[i].attr('id') != "btn-maul") {
+                //players[i].hide();
+                players[i].appendTo("#enemy-area")
+            }
+        }
+
+        // for (var i = 0; i < enemies.length; i++) {
+        //     if (enemies[i].attr('id') != "enemy-maul-back") {
+        //         enemies[i].show();
+        //     }
+        // }
+    });
+
+    */
+
 });
 
 /*  
