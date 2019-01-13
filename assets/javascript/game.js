a69ad15972cbacf4a28fdcde31e40201b95ac908
Raymond Fwu <rfwu@raymonds-imac.attlocal.net>
2019-01-13 01:14:30 -0600

    significant changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,3 @@
-// Step 1
-// Make all the characters selectable
-
-// var student = {                 // object name
-//     firstName:"Jane",           // list of properties and values
-//     lastName:"Doe",
-//     age:18,
-//     height:170,
-//     fullName : function() {     // object function
-//        return this.firstName + " " + this.lastName;
-//     }
-// }; 
-
 $(document).ready(function(){
 
     function Character(pChar, pColor, pHP, hitPoints, attackPower, counterAttackPower) {
@@ -23,6 +10,7 @@ $(document).ready(function(){
         // variables for each character's attributes
         this.hitPoints          = hitPoints,
         this.attackPower        = attackPower,
+        this.levelUp            = attackPower,
         this.counterAttackPower = counterAttackPower,
 
         this.updateHP = function() {
@@ -35,100 +23,90 @@ $(document).ready(function(){
         this.enemySelect = function() {
             pChar.appendTo("#enemy-area");
             pColor.removeClass("bg-white");
+            pColor.removeClass("bg-dark");
             pColor.addClass("bg-danger");
         }
+
+        this.enemyFight = function() {
+            pChar.appendTo("#defender-area");
+            pChar.addClass("text-light");
+            pColor.removeClass("bg-white");
+            pColor.removeClass("bg-danger");
+            pColor.addClass("bg-dark");
+        }
     }
 
-    var kenobi    = new Character($("#btn-kenobi"), $("#btn-kenobi-back"), $("#kenobi-hp"), 120, 6, 15);
-    var skywalker = new Character($("#btn-skywalker"), $("#btn-skywalker-back"), $("#skywalker-hp"), 100, 5, 10);
+    var kenobi    = new Character($("#btn-kenobi"), $("#btn-kenobi-back"), $("#kenobi-hp"), 120, 8, 6);
+    var skywalker = new Character($("#btn-skywalker"), $("#btn-skywalker-back"), $("#skywalker-hp"), 100, 10, 5);
     var sidious   = new Character($("#btn-sidious"), $("#btn-sidious-back"), $("#sidious-hp"), 150, 8, 20);
-    var maul      = new Character($("#btn-maul"), $("#btn-maul-back"), $("#maul-hp"), 180, 9, 25);
+    var maul      = new Character($("#btn-maul"), $("#btn-maul-back"), $("#maul-hp"), 180, 3, 25);
 
-    // put all the characters into an array to quickly access all of them in one statement
     var players = [kenobi, skywalker, sidious, maul];
 
-    for (let i = 0; i < players.length; i++) {
-        console.log(players[i].pID);
-        players[i].pChar.click(function() {
-            for (let j = 0; j < players.length; j++) {
-                if (players[j] != players[i]) {
-                    players[j].enemySelect();
+    var score = 0;
+
+    var PlayerSelect = {
+        // Present a bunch of characters to choose from
+        // First set an onclick listener to each of them.
+        // When the player clicks, move the other characters to the enemy area.
+        // Remove the listeners and then set new listeners to the enemy characters.
+
+        thePlayer: "",
+        select: function(thePlayer, players) {
+            this.thePlayer = thePlayer;
+            for (let i = 0; i < players.length; i++) {
+                players[i].pChar.off();
+                if (thePlayer != players[i]) {
+                    players[i].enemySelect();
+                    players[i].pChar.click(function() {
+                        for (let j = 0; j < players.length; j++) {
+                            players[j].pChar.off();
+                        }
+                        Fight.clash(thePlayer, players[i], players);
+                    })
                 }
             }
-        })
+        }
     }
 
-    /*
-
-    //make an array of objects for the buttons and enemies
-    var players = [$("#btn-kenobi"), $("#btn-skywalker"), $("#btn-sidious"), $("#btn-maul")];
-    var enemies = [$("#enemy-kenobi-back"), $("#enemy-skywalker-back"), $("#enemy-sidious-back"), $("#enemy-maul-back")];
-
-    $("#btn-kenobi").click(function(){
-        for (var i = 0; i < players.length; i++) {
-            if (players[i].attr('id') != "btn-kenobi") {
-                //players[i].hide();
-                players[i].appendTo("#enemy-area")
-            }
+    var Fight = {
+        clash: function(thePlayer, theEnemy, players) {
+            theEnemy.enemyFight();
+            $("#fightBtn").click(function() {
+                $("#actions").text("");
+                theEnemy.hitPoints -= thePlayer.attackPower;
+                theEnemy.updateHP();
+                $("#actions").append("<p>You attacked for " + thePlayer.attackPower + " damage.</p>");
+                thePlayer.attackPower += thePlayer.levelUp;
+                if (theEnemy.hitPoints <= 0) {
+                    score++;
+                    if (score >= players.length-1) {
+                        $("#actions").html("<p>You won!...GAME OVER</p>");
+                        $("#fightBtn").off();
+                    } else {
+                        $("#actions").html("<p>The enemy has been defeated, choose another enemy</p>");
+                        theEnemy.pChar.hide();
+                        $("#fightBtn").off();
+                        PlayerSelect.select(thePlayer, players);
+                    }
+                } else {
+                    thePlayer.hitPoints -= theEnemy.counterAttackPower;
+                    thePlayer.updateHP();
+                    if (thePlayer.hitPoints <=0) {
+                        $("#actions").html("<p>You've been defeated...GAME OVER</p>");
+                        $("#fightBtn").off();
+                    } else {
+                        $("#actions").append("<p>The enemy counterattacked with " + theEnemy.counterAttackPower + " damage.</p>");
+                    }
+                }
+            })
         }
+    }
 
-        // for (var i = 0; i < enemies.length; i++) {
-        //     if (enemies[i].attr('id') != "enemy-kenobi-back") {
-        //         enemies[i].show();
-        //     }
-        // }
-    });
-
-    $("#btn-skywalker").click(function(){
-        for (var i = 0; i < players.length; i++) {
-            if (players[i].attr('id') != "btn-skywalker") {
-                //players[i].hide();
-                players[i].appendTo("#enemy-area")
-            }
-        }
-
-        // for (var i = 0; i < enemies.length; i++) {
-        //     if (enemies[i].attr('id') != "enemy-skywalker-back") {
-        //         enemies[i].show();
-        //     }
-        // }
-    });
-
-    $("#btn-sidious").click(function(){
-        for (var i = 0; i < players.length; i++) {
-            if (players[i].attr('id') != "btn-sidious") {
-                //players[i].hide();
-                players[i].appendTo("#enemy-area")
-            }
-        }
-
-        // for (var i = 0; i < enemies.length; i++) {
-        //     if (enemies[i].attr('id') != "enemy-sidious-back") {
-        //         enemies[i].show();
-        //     }
-        // }
-    });
-
-    $("#btn-maul").click(function(){
-        for (var i = 0; i < players.length; i++) {
-            if (players[i].attr('id') != "btn-maul") {
-                //players[i].hide();
-                players[i].appendTo("#enemy-area")
-            }
-        }
-
-        // for (var i = 0; i < enemies.length; i++) {
-        //     if (enemies[i].attr('id') != "enemy-maul-back") {
-        //         enemies[i].show();
-        //     }
-        // }
-    });
-
-    */
-
+    for (let i = 0; i < players.length; i++) {
+        players[i].pChar.click(function() {
+            PlayerSelect.select(players[i], players);
+            console.log("click");
+        })
+    }
 });
-
-/*  
-    Step 2
-    Move the selected character's portrait into the "Your Character" section
-*/
